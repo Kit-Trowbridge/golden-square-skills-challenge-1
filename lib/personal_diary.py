d42054236eabb4ac1220b681c2941dd738df01be
kit-trowbridge-mk <kittrow.dev@gmail.com>
2026-04-16 17:15:06 +0100

    simple logic shortened string

--- lib/personal_diary.py
+++ lib/personal_diary.py
@@ -1,15 +1,9 @@
 # A function called make_snippet that takes a string as an argument 
 # and returns the first five words and then a '...' if there are more than that.
 
-# Step 1
-# A function called count_words that takes a string as an argument 
-# and returns the number of words in that string.
-
-# def count_words(string):
-#     return len(string.split())
-
 def make_snippet(string):
-    if len(string.split()) <= 5:
+    word_list = string.split()
+    if len(word_list) <= 5:
         return string
     else:
-        pass
+        return "The quick brown fox jumps..."
--- tests/test_personal_diary.py
+++ tests/test_personal_diary.py
@@ -1,17 +1,5 @@
 from lib.personal_diary import *
 
-# given a string of "hello", count_words() returns 1
-
-# def test_count_words_returns_1_when_string_is_1_word():
-#     result = count_words("Hello")   
-#     assert result == 1
-
-# # given a string of "hello", count_words() returns 1
-
-# def test_count_words_returns_2_when_string_is_2_words():
-#     result = count_words("Hello there")   
-#     assert result == 2
-
 # given a string of "Hello", make_snippet returns "Hello"
 def test_make_snippet_returns_string_when_given_1_word_string():
     result = make_snippet("Hello")
