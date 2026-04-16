900b3388bbee2a926d0cdef880587349162a8f7e
kit-trowbridge-mk <kittrow.dev@gmail.com>
2026-04-16 12:35:44 +0100

    more small steps

--- tests/test_personal_diary.py
+++ tests/test_personal_diary.py
@@ -1,6 +1,8 @@
 # A function called make_snippet that takes a string as an argument 
 # and returns the first five words and then a '...' if there are more than that.
 
+from lib.personal_diary import *
+
 # Given a string, it returns a string
 def test_make_snippet_returns_string():
     result = make_snippet("Hello")
