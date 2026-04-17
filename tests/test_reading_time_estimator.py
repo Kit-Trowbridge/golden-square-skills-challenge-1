102a6628ceb049dfbc720920b1b97a1a4f72eede
kit-trowbridge <kittrow.dev@gmail.com>
2026-04-17 12:17:54 +0100

    second test fail

--- tests/test_reading_time_estimator.py
+++ tests/test_reading_time_estimator.py
@@ -1,10 +1,23 @@
 from lib.reading_time_estimator import *
+
 """
 Given an empty string
 It returns 0
-estimate_reading_time("") => 0.0
 
 """
 def test_estimate_reading_time_returns_0_for_empty_string():
     result = estimate_reading_time("")
     assert result == 0
+
+"""
+Given a string of exactly 200 words
+It returns 1
+"""
+
+def test_estimate_reading_time_returns_1_for_200_words():
+    result = estimate_reading_time(two_hundred_word_text)
+    assert result == 1
+
+# Texts 
+
+two_hundred_word_text = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way--in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only. There were a king with a large jaw and a queen with a plain face, on the throne of England; there were a king with a large jaw and a queen with a fair face, on the throne of France. In both countries it was clearer than crystal to the lords of the State preserves of loaves and fishes, that things in general were settled for ever. It was the year of Our Lord one thousand seven hundred and seventy-five."
